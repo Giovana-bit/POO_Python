3658401afa2b8664da90648242eb0077c889aaf9
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-09-02 22:23:19 -0300

    tela de listagem, consulta, retirada do ID

--- at02/home.py
+++ at02/home.py
@@ -1,5 +1,7 @@
+
 from PyQt5 import uic, QtWidgets
 import mysql.connector
+import os
 
 # Conexão com o banco de dados
 banco = mysql.connector.connect(
@@ -39,10 +41,44 @@ def main():
     banco.commit()
     print("✅ Registro inserido com sucesso!")
 
+    # Limpando os campos após salvar
+    agenda.nomeAluno.setText("")
+    agenda.emailAluno.setText("")
+    agenda.telefoneAluno.setText("")
+
+    print("Campos limpos.")
+
+# Função para abrir tela de contatos
+def telaConsulta():
+    listaContatos.show()
+
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM contatos"
+    cursor.execute(comando_SQL)
+    contatosLidos = cursor.fetchall()
+    
+    listaContatos.tableWidget.setRowCount(len(contatosLidos))
+    listaContatos.tableWidget.setColumnCount(5)
+
+    for i in range(0, len(contatosLidos)):
+        for j in range(0, 5):
+            listaContatos.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(contatosLidos[i][j])))
+
 # Configuração da aplicação
 app = QtWidgets.QApplication([])
-agenda = uic.loadUi("agenda.ui")
+
+# Descobre automaticamente o caminho dos arquivos .ui
+base_dir = os.path.dirname(os.path.abspath(__file__))
+ui_path = os.path.join(base_dir, "..", "at02", "agenda.ui")
+ui_consulta = os.path.join(base_dir, "..", "at02", "listaContatos.ui")
+
+# Carrega as duas telas em variáveis diferentes
+agenda = uic.loadUi(ui_path)
+listaContatos = uic.loadUi(ui_consulta)
+
+# Ligações de eventos
 agenda.cadastrar.clicked.connect(main)
+agenda.consultar.clicked.connect(telaConsulta)
 
 agenda.show()
 app.exec()
